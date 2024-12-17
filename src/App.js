82db23b0126ca2afc8c6cd88467f8d52f4ea3a96
Naveen Horatti <navidon2@gmail.com>
2024-12-16 19:51:41 -0500

    Made text changes to my react project

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello!! Welcome to my page :)</p>
+        <p>Hello!! Welcome to my page Lets learn Devops Together</p>
         <a
           className="App-link"
           href="https://reactjs.org"
